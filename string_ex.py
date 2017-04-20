dec456b7c355ae2b52e8326b807f3d601afa5289
Stephen Ward <stephenward@Stephens-MBP.attlocal.net>
2017-04-19 22:02:48 -0400

    String Exercises

---
+++ string_ex.py
@@ -0,0 +1,83 @@
+# stringer = "the string exercises"
+# print (stringer.upper())
+# print (stringer.title())
+
+ # stringer = "the string exercises"
+# up_string = ""
+# for stringer in stringer:
+# 	up_string += stringer.upper()
+# print up_string
+
+
+# reverse_list = stringer.split(" ")
+# print reverse_list [::-1]
+
+# Leetspeak
+# p_graph = ("There was an old lady who lived in a shoe.").upper()
+# result = ""
+# for letter in p_graph:
+# 	if (letter == "A"):
+# 		result =  result + "4"
+# 	if (letter == "E"):
+# 		result =  result + "3"
+# 	if (letter == "G"):
+# 		result =  result + "6"
+# 	if (letter == "I"):
+# 		result =  result + "1"
+# 	if (letter == "O"):
+# 		result =   result + "0"
+# 	if (letter == "S"):
+# 		result =  result + "5"
+# 	if (letter == "T"):
+# 		result =  result + "7"
+# 	else:
+# 		result = result + letter
+
+# print result
+
+
+
+
+# Long Vowels
+vowels = ["a", "e", "i", "o", "u"]
+long_vow_word = "Good"
+count = 0
+
+for letter in long_vow_word:
+	if letter in vowels:
+		count += 1
+		if count > 1:
+			long_list = list(long_vow_word)
+			vowel_index = long_list.index(letter)
+			long_list.remove(letter)
+			long_list.insert(vowel_index, letter*4)
+			new_vow_word = "".join(long_list)
+
+print (new_vow_word)
+
+
+	
+
+
+# stringer = "the string exercises"
+# up_string = ""
+# for stringer in stringer:
+# 	up_string += stringer.upper()
+# print up_string
+
+
+
+
+# stringer = "the string exercises"
+# stringer_whole = len(stringer)
+# cap_string = ""
+# for stringer in stringer:
+# print cap_string
+
+
+# print (stringer.upper())
+# print (stringer.title())
+
+# stringer = ["The string exercises"]
+
+
